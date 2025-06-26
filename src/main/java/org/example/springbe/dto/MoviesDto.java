a0b5008be22ece6d5e0f97eb484bdb2b975a28de
DESKTOP-8MAM7KK\wd <bambin25>
2025-06-26 16:07:16 +0900

    first commit

---
+++ src/main/java/org/example/springbe/dto/MoviesDto.java
@@ -0,0 +1,13 @@
+package org.example.springbe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class MoviesDto {
+    private List<MovieDto> movies;
+}
+
